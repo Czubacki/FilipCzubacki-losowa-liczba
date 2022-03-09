02bad098768726c29b804dfcdbb724c7a2dd92e9
Acer to gowno <Acer to gowno@DESKTOP-115IQLU>
2022-03-09 16:47:22 +0100

    Add project files.

---
+++ FilipCzubacki losowa liczba/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FilipCzubackiLiczba
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 10;
+            int zgadnij;
+            int liczba;
+            int zgadles;
+
+            while (playAgain)
+            {
+                zgadnij = 0;
+                zgadles = 0;
+                liczba = random.Next(min, max + 1);
+
+                while (zgadnij != liczba)
+                {
+                    Console.WriteLine("Zgadnij liczbe " + min + " - " + max + " : ");
+                    zgadnij = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Zgadnij: " + zgadnij);
+
+                    if (zgadnij > liczba)
+                    {
+                        Console.WriteLine(zgadnij + " za wysoka");
+                    }
+                    else if (zgadnij < liczba)
+                    {
+                        Console.WriteLine(zgadnij + " za niska");
+                    }
+                    zgadles++;
+                }
+                Console.WriteLine("Liczba: " + liczba);
+                Console.WriteLine("Udalo ci sie");
+
+            }
+        }
+    }
+}
